e196da85f0f80e219c31fdca35595903da4108f7
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-03-15 23:02:20 -0700

    Initial Commit

---
+++ MyBusinessCard/MyBusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBusinessCardApp.swift
+//  MyBusinessCard
+//
+//  Created by Ikbal Demirdoven on 2023-03-15.
+//
+
+import SwiftUI
+
+@main
+struct MyBusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
